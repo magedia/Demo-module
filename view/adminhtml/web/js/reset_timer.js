3a4a0f16bfae85743940c097c88ee672cb48a81d
vitaminka-007 <pavelhrytsuk@magedia.com>
2022-01-05 13:31:57 +0300

    resfresh warning window

--- view/adminhtml/web/js/reset_timer.js
+++ view/adminhtml/web/js/reset_timer.js
@@ -9,17 +9,23 @@ define([
         let resetTimeout = config.timerSettings.reset_timeout;
         const nextResetTime = Date.parse(lastResetTime) + resetTimeout * 60 * 1000;
         let timeToReset = (nextResetTime - currentTimeMilliseconds) / 1000;
+        const bodyTag = $('body');
+
+        bodyTag.removeClass('active-reload');
 
         $('#reset_timeout').text(resetTimeout);
         let timer = setInterval(function tick (){
-            if(timeToReset > 0 && timeToReset !== 0){
+            if(timeToReset !== 0 && timeToReset > 10){
                 timeToReset = timeToReset - 1;
                 let minuteToReset = Math.floor(timeToReset / 60);
                 let secondToReset = timeToReset % 60;
                 $('#reset_timer').text(`${minuteToReset < 10 ? '0' + minuteToReset : minuteToReset}:${secondToReset < 10 ? '0' + secondToReset : secondToReset}`);
             }else{
                 clearInterval(timer);
-                document.location.reload();
+                bodyTag.addClass('active-reload')
+                setTimeout(function (){
+                    document.location.reload();
+                }, 30000)
             }
         }, 1000);
     }
